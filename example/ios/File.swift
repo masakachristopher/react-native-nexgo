62f86afc95de8b80ecade2bbced694360c3eec79
masakachristopher <masakachristopher@yahoo.com>
2022-08-24 13:24:46 +0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  NexgoExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-nexgo': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
